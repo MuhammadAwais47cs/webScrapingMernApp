3f1f0c6c9241f02d628d57b9ab525cf8d24b15b1
MuhammadAwais47cs <Muhammadawais47cs@gmail.com>
2023-02-24 01:50:47 +0500

    add baseurl file

--- client/src/actions/productAction.js
+++ client/src/actions/productAction.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { baseurl } from '../baseurl';
 import {
      ALL_PRODUCT_REQUEST,ALL_PRODUCT_SUCCESS,ALL_PRODUCT_FAIL,
      PRODUCT_DETAILS_REQUEST,PRODUCT_DETAILS_SUCCESS,PRODUCT_DETAILS_FAIL,
@@ -10,14 +11,15 @@ export const getProduct =
  async (dispatch) => {
     try {
         dispatch({type: ALL_PRODUCT_REQUEST});
-      let link = `http://localhost:4000/api/v1/products?keyword=${keyword}&page=${currentPage}`;
+      let link = `${baseurl}/api/v1/products?keyword=${keyword}&page=${currentPage}`;
+    //   let link = `http://localhost:4000/api/v1/products?keyword=${keyword}&page=${currentPage}`;
     //   let link = `http://localhost:4000/api/v1/products?keyword=${keyword}&page=${currentPage}&price[gte]=${price[0]}&price[lte]=${price[1]}&ratings[gte]=${ratings}`;
     console.log('category :>> ', category);
       if (category) {
-        link = `http://localhost:4000/api/v1/products?keyword=${keyword}&page=${currentPage}&category=${category}`;
+        link = `${baseurl}/api/v1/products?keyword=${keyword}&page=${currentPage}&category=${category}`;
       }
       if (brand) {
-        link = `http://localhost:4000/api/v1/products?keyword=${keyword}&page=${currentPage}&brand=${brand}`;
+        link = `${baseurl}/api/v1/products?keyword=${keyword}&page=${currentPage}&brand=${brand}`;
       }
 
         const {data} = await axios.get(link);
@@ -38,7 +40,7 @@ export const getProductDetails = (id) => async (dispatch) => {
     try {
         console.log('id :>> ', id);
         dispatch({type: PRODUCT_DETAILS_REQUEST});
-        const {data} = await axios.get(`http://localhost:4000/api/v1/product/${id}`)
+        const {data} = await axios.get(`${baseurl}/api/v1/product/${id}`)
         dispatch({
             type:PRODUCT_DETAILS_SUCCESS,
             payload: data.product,
---
+++ client/src/baseurl.js
@@ -0,0 +1,2 @@
+module.exports.baseurl = "https://web-scraping-mern-app.herokuapp.com"; // Dealers Book Live Server
+
